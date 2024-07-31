fe0832832a1168cc6dfcd7c2ea68dbca7829f185
Prachi Lad <prachilad061@gmail.com>
2024-08-01 00:35:01 +0530

    add new files

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer","Curious to learn new Things"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+
+ 
